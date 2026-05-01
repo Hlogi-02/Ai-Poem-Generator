68d16466a8b6c4d789134aba6255f758149bdf17
Hlogi-02 <mahlogonolo059@gmail.com>
2026-05-01 16:42:57 +0200

    Added AI API integration

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -14,9 +13,9 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "300t5e9acdfe4e9o84a93ca0d669aabd";
   let context =
-    "You are an expert in writing short heartfelt poems. Your mission is to generate 4 line poem in basic HTML and seperate each line with a <br />. Make sure to follow user instructions. Do not include a title to the poem. Sign the poem with 'Hlogi AI' inside a <strong> element at the end of the poem and NoOT at the beginning";
+    "You are an expert in writing short heartfelt poems. Your mission is to generate 4 line poem in basic HTML and seperate each line with a <br />. Make sure to follow user instructions. Do not include a title to the poem. Sign the poem with 'Hlogi AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt =
-    "User instructions: Generate a heartfelt poem ${instructionsInput.value}";
+    "User instructions: Generate an english poem about  ${instructionsInput.value}";
 
   let apiURL =
     "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
